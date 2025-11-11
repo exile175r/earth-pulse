ebaeda8b01e2b8bd65acd4d099e8f8b30a985f2d
이희승 <exile175r@gmail.com>
2025-11-12 00:29:30 +0900

    존재하지 않는 babel-plugin-react-compiler 패키지 제거

--- web/vite.config.js
+++ web/vite.config.js
@@ -3,13 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [
-    react({
-      babel: {
-        plugins: [
-          ['babel-plugin-react-compiler', {}],
-        ],
-      },
-    }),
+    react(),
   ],
   server: {
     port: 5173,
